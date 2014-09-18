dd61179a199d6067d781f70c20614e5fdc52e419
Mustafa Faraj <vis.mustafa@gmail.com>
2014-09-17 22:24:20 -0400

    Added memory write logic
    Added the memory write logic for the three different sizes of access (32
    bit, 16 bit and 8 bit). Not very sure that the buffer shifting method
    works (check TODO).Will need to test this

--- memory.v
+++ memory.v
@@ -2,10 +2,11 @@ module memory(
         output reg [31:0] data_out,
         input [31:0] address,
         input [31:0] data_in, 
-        input writing,
+        input write,
         input clk,
 		input [1:0] access_size
 );
+    reg [31:0] buffer;
 	parameter size = 1048577;
 	parameter offset = 2147614720 ;  // 0x80020000 in decimal
 	
@@ -20,8 +21,30 @@ module memory(
 	
 	// writing
 	always @(posedge clk) begin
-		if (writing) begin    
+		if (write) begin    
 			//Writing logic
+            if (access_size == '10') begin //32 bit access
+            	buffer = data_in;
+				memory[address - offset] = buffer; //TODO: will this work, or do we need to clip the buffer to 8 bits before writing it to memory?
+				buffer = buffer >> 8;
+				memory[address - offset + 1] = buffer;
+				buffer = buffer >> 8;
+				memory[address - offset + 2] = buffer;
+				buffer = buffer >> 8;
+				memory[address - offset + 3] = buffer;
+            end
+            
+            if (access_size == '01') begin //16 bit access 
+                buffer = data_in;
+                memory[address - offset] = buffer;
+                buffer = buffer >> 8;
+                memory[address - offser + 1] = buffer;
+            end
+            
+            if (access_size == '00') begin //8 bit access
+                buffer = data_in;
+                memory[address - offset] = buffer;
+            end
 		end
 		
 	end
