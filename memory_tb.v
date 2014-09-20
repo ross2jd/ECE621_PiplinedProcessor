4b3c035539413d509ef18a81238e1caec17258e9
Jordan Ross <ross.j101@gmail.com>
2014-09-20 10:52:09 -0400

    Added test cases to memory_tb.v
    Added test cases for reading and writing 8 -bit and 16-bit values to
    memory and reading them back. Test cases pass for memory.v

--- memory_tb.v
+++ memory_tb.v
@@ -12,6 +12,7 @@
 // 
 // Revision:
 // 0.01 - File Created and Initial Tests
+// 0.02 - Added test cases.
 // Additional Comments:
 //
 ///////////////////////////////////////////////////////////////////////////////
@@ -87,6 +88,54 @@ module memory_tb;
         #50; // delay until falling edge
         // read the data line on the falling edge;
         $monitor("The 8 bit value in the first instruction address space is 0x%h\n", data_out[7:0]);
+        #50;
+        
+        // Now we are going to test writing a 16-bit value and then reading it back.
+        // We want to test writing an value to address 0x80020008 for a program instruction
+        $monitor("Testing writing a 16 bit value into memory...\n");
+        $monitor("Writing the value 0xAAAA to address 0x80020008...\n");
+        // Set the address line on the rising edge;
+        address = instruction_offset+8;
+        write = 1;
+        #50; // delay until falling edge
+        // Set the data line on the falling edge;
+        data_in = 16'hAAAA;
+        access_size = 2'b01;
+        #50;
+        
+        #100; // wait 1 clock cycle.
+         
+        $monitor("Testing reading the 16 bit value from 0x80020008...\n");
+        address = instruction_offset+8;
+        write = 0;
+        access_size = 2'b01;
+        #50; // delay until falling edge
+        // read the data line on the falling edge;
+        $monitor("The 16 bit value is 0x%h\n", data_out);
+        #50;
+        
+        // Now we are going to test writing a 8-bit value and then reading it back.
+        // We want to test writing an value to address 0x8002000D for a program instruction
+        $monitor("Testing writing a 8 bit value into memory...\n");
+        $monitor("Writing the value 0xBB to address 0x8002000D...\n");
+        // Set the address line on the rising edge;
+        address = instruction_offset+12;
+        write = 1;
+        #50; // delay until falling edge
+        // Set the data line on the falling edge;
+        data_in = 8'hBB;
+        access_size = 2'b00;
+        #50;
+        
+        #100; // wait 1 clock cycle.
+         
+        $monitor("Testing reading the 8 bit value from 0x8002000D...\n");
+        address = instruction_offset+12;
+        write = 0;
+        access_size = 2'b00;
+        #50; // delay until falling edge
+        // read the data line on the falling edge;
+        $monitor("The 8 bit value is 0x%h\n", data_out);
         #50;
         
 	end
